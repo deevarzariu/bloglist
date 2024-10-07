53bf2b8abfcc33b0da5d5193a83754ada1be8152
deevarzariu <dell.varzariu@gmail.com>
2024-10-07 23:56:33 +0300

    Add users route and model

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
 
 const { MONGODB_URI } = require("./utils/config");
 
 const blogRouter = require("./controllers/blog");
+const userRouter = require("./controllers/user");
 
 mongoose.connect(MONGODB_URI);
 
@@ -13,5 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
+app.use(middleware.errorHandler);
 
 module.exports = app;
